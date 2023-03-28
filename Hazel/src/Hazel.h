5ce281ae5317a0d510f0720bb169053f07542b01
yunlong <502765299@qq.com>
2023-03-28 20:13:28 +0800

    +Hazel EntryPoint

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdio>
+
+// For use by Hazel applications
+#include "Hazel/Application.h"
+
+// ---Entry Point------------
+#include "Hazel/EntryPoint.h"
+// --------------------------
+
