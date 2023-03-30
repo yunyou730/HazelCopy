ed0de9db55d8c8aa1709dd103ac11e8d4b4461d8
yunlong <502765299@qq.com>
2023-03-30 22:40:29 +0800

    +integrate premake

---
+++ premake5.lua
@@ -0,0 +1,13 @@
+workspace "Hazel"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+project "Hazel"
+
+    HZ_PLATFORM_WINDOWS;HZ_BUILD_DLL;_WINDLL;
