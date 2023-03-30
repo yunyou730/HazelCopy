84cf6139aa0089a6f0bbb3b8f06b631e43bdc9f1
yunlong <502765299@qq.com>
2023-03-30 22:57:09 +0800

    +update premake

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,30 @@ workspace "Hazel"
         "Dist"
     }
 
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
 project "Hazel"
+    location "Hazel"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/${prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/${prj.name}")
+
+    files
+    {
+        "${prj.name}/src/**.h",
+        "${prj.name}/src/**.cpp"
+    }
+
+    include
+    {
+        "${prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
 
     HZ_PLATFORM_WINDOWS;HZ_BUILD_DLL;_WINDLL;
